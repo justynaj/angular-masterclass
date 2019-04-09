2c80ba6027a116805ec66324e5aef97e0b247880
Justyna Jóźwik <just.jozwik@gmail.com>
2019-04-09 12:25:26 +0200

    feat(homepage): create component

--- facebook/src/app/core/core.module.ts
+++ facebook/src/app/core/core.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { CoreRoutingModule } from './core-routing.module';
 import { AppComponent } from './components/app/app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomePageComponent],
   imports: [BrowserModule, CoreRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
