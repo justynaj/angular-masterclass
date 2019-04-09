4e7f488a48bc3dc56219dea4b25d599940a402a4
Justyna Jóźwik <just.jozwik@gmail.com>
2019-04-09 13:42:40 +0200

    feat(posts): create post list component and module

--- facebook/src/app/core/core.module.ts
+++ facebook/src/app/core/core.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { CoreRoutingModule } from './core-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PostsModule } from '../posts/posts.module';
 
 @NgModule({
   declarations: [AppComponent, HomePageComponent],
-  imports: [BrowserModule, CoreRoutingModule],
+  imports: [BrowserModule, CoreRoutingModule, PostsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ facebook/src/app/posts/posts.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PostListComponent } from './components/post-list/post-list.component';
+
+@NgModule({
+  declarations: [PostListComponent],
+  imports: [CommonModule],
+  exports: [PostListComponent]
+})
+export class PostsModule {}
