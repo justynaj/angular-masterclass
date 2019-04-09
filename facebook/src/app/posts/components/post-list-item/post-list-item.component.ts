862edbc97651dde54007b4370fda3d8da2179ed7
Justyna Jóźwik <just.jozwik@gmail.com>
2019-04-09 15:59:48 +0200

    feat(posts): enable onpush strategy in changedetector

--- facebook/src/app/posts/components/post-list-item/post-list-item.component.ts
+++ facebook/src/app/posts/components/post-list-item/post-list-item.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { Post } from '../../interfaces/post.interface';
 
 @Component({
   selector: 'app-post-list-item',
   templateUrl: './post-list-item.component.html',
-  styleUrls: ['./post-list-item.component.scss']
+  styleUrls: ['./post-list-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PostListItemComponent implements OnInit {
   @Input() post: Post = null;
