6810e9f882c086f68f129a8ab52f125855faca66
Justyna Jóźwik <just.jozwik@gmail.com>
2019-04-10 16:40:20 +0200

    chore(performance): unsubscribe with takeUntil

--- facebook/src/app/users/pages/users/users-page.component.ts
+++ facebook/src/app/users/pages/users/users-page.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 import { User } from '../../../shared/interfaces/user.interface';
 import { UsersService } from '../../services/users.service';
@@ -9,24 +12,32 @@ import { UsersService } from '../../services/users.service';
   templateUrl: './users-page.component.html',
   styleUrls: ['./users-page.component.scss']
 })
-export class UsersPageComponent implements OnInit {
+export class UsersPageComponent implements OnInit, OnDestroy {
   public user: User = null;
+  destroy$ = new Subject();
 
   constructor(private usersService: UsersService, private route: ActivatedRoute) {}
 
-  ngOnInit() {
+  public ngOnInit() {
     this.setupUser();
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next(null);
   }
 
   private setupUser(): void {
     const userId = this.route.snapshot.params.userId;
-    this.usersService.getUserById(userId).subscribe({
-      next: response => {
-        this.user = response;
-      },
-      error: err => {
-        console.log(err);
-      }
-    });
+    this.usersService
+      .getUserById(userId)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: response => {
+          this.user = response;
+        },
+        error: err => {
+          console.log(err);
+        }
+      });
   }
 }
