d5ded3c6e80bab8da8fa8d943fa4a780e674647b
Justyna Jóźwik <just.jozwik@gmail.com>
2019-04-10 09:44:29 +0200

    Updates

--- main.js
+++ main.js
@@ -454,7 +454,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 var environment = {
     production: false,
-    postsUrl: '/assets/posts.json'
+    postsUrl: 'assets/posts.json'
 };
 /*
  * For easier debugging in development mode, you can import the following file
